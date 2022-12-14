25ef2a048963cf4fa0fcb5b78ad1e7746237d1e5
justinwuzijin <justinwu137@gmail.com>
2022-12-14 14:07:14 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -13,8 +13,10 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+
     public MyWorld()
     {    
+        //lol
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
     }
